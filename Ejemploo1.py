5b4e1ee0324976d579fde936cdf6c3e14fb52092
MORIBELITO <74902137@continental.edu.pe>
2024-04-10 17:07:10 -0500

    Malos Habitos

---
+++ Ejemploo1.py
@@ -0,0 +1,12 @@
+def f(PrimerFactor, SegundoFactor):
+
+    Producto = PrimerFactor * SegundoFactor
+    return Producto
+
+    PrimerNumero = 10
+    SegundoNumero = 5
+    z = PrimerNumero + SegundoNumero
+
+    resultado = f(PrimerNumero, z)
+
+    print(resultado)
---
+++ Ejemploo2.py
@@ -0,0 +1,13 @@
+def calcular(FactorUno, FactorDos, FactorTres):
+
+    return (FactorUno * FactorDos) + FactorTres
+
+def principal():
+
+    VariableUno = 5
+    VariableDos = 3
+    VariableTres = 7
+    resultado = calcular(VariableUno, VariableDos, VariableTres)
+    print("El resultado es:", resultado)
+
+principal()
